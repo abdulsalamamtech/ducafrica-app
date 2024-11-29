a9e3e2d9e462345a438ca8076f0a66add7895607
abdulsalamamtech <abdulsalamamtech@gmail.com>
2024-11-29 15:22:22 +0100

    Add my-dashboard view for user

---
+++ resources/views/dashboard/my-dashboard.blade.php
@@ -0,0 +1,236 @@
+@extends('dashboard.layouts.master')
+
+@section('content')
+    <!-- Container -->
+    <div class="p-4 sm:ml-64">
+
+        <div class="rounded-lg dark:border-gray-700 mt-20">
+
+            {{-- Information --}}
+            {{-- <div
+                class="bg-white shadow-lg rounded-lg flex items-center gap-2 p-4 w-full dark:bg-slate-800 dark:text-gray-100">
+                <div class="md:p-2">
+                    <i class="fa fa-truck bg-[#0828b9] p-2 rounded-full md:text-2xl text-white"></i>
+                </div>
+                <div class="sm:p-2 mb-4 md:mb-0">
+                    <h2 class="md:text-xl font-bold mb-2">Amtech Oil & Gas</h2>
+                    <p class="text-gray-700 dark:text-slate-400 mb-1">manager@amtechoilandgas.com</p>
+                    <p class="text-gray-500 dark:text-gray-300">No. 54 Rimi, Gwarzo Road, Kano State</p>
+                </div>
+            </div> --}}
+
+            {{-- Statistica widge --}}
+            <div class="p-6 my-6 bg-white rounded-lg shadow-lg dark:bg-gray-800">
+                <div class="grid gap-y-10 gap-x-6 md:grid-cols-2 xl:grid-cols-4 mb-4">
+                    <div
+                        class="dark:bg-gray-700 dark:text-gray-300 dark:border-gray-700 relative flex flex-col bg-clip-border rounded-xl bg-white text-gray-700 border border-blue-gray-100 shadow-sm">
+                        <div
+                            class="bg-clip-border mt-4 mx-4 rounded-xl overflow-hidden bg-gradient-to-tr from-gray-900 to-gray-800 text-white shadow-gray-900/20 absolute grid h-12 w-12 place-items-center">
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor"
+                                aria-hidden="true" class="w-6 h-6 text-white">
+                                <path d="M12 7.5a2.25 2.25 0 100 4.5 2.25 2.25 0 000-4.5z"></path>
+                                <path fill-rule="evenodd"
+                                    d="M1.5 4.875C1.5 3.839 2.34 3 3.375 3h17.25c1.035 0 1.875.84 1.875 1.875v9.75c0 1.036-.84 1.875-1.875 1.875H3.375A1.875 1.875 0 011.5 14.625v-9.75zM8.25 9.75a3.75 3.75 0 117.5 0 3.75 3.75 0 01-7.5 0zM18.75 9a.75.75 0 00-.75.75v.008c0 .414.336.75.75.75h.008a.75.75 0 00.75-.75V9.75a.75.75 0 00-.75-.75h-.008zM4.5 9.75A.75.75 0 015.25 9h.008a.75.75 0 01.75.75v.008a.75.75 0 01-.75.75H5.25a.75.75 0 01-.75-.75V9.75z"
+                                    clip-rule="evenodd"></path>
+                                <path
+                                    d="M2.25 18a.75.75 0 000 1.5c5.4 0 10.63.722 15.6 2.075 1.19.324 2.4-.558 2.4-1.82V18.75a.75.75 0 00-.75-.75H2.25z">
+                                </path>
+                            </svg>
+                        </div>
+                        <div class="p-4 text-right">
+                            <p class="block antialiased font-sans text-sm leading-normal font-normal text-blue-gray-600">
+                                Total Transactions
+                            </p>
+                            <h4
+                                class="block antialiased tracking-normal font-sans text-2xl font-semibold leading-snug text-blue-gray-900">
+                                ₦503k</h4>
+                        </div>
+                        <div class="dark:border-gray-500 border-t border-blue-gray-50 p-4">
+                            <p class="block antialiased font-sans text-base leading-relaxed font-normal text-blue-gray-600">
+                                <strong class="text-green-500">+55%</strong>&nbsp;than last week
+                            </p>
+                        </div>
+                    </div>
+                    <div
+                        class="dark:bg-gray-700 dark:text-gray-300 dark:border-gray-700 relative flex flex-col bg-clip-border rounded-xl bg-white text-gray-700 border border-blue-gray-100 shadow-sm">
+                        <div
+                            class="bg-clip-border mt-4 mx-4 rounded-xl overflow-hidden bg-gradient-to-tr from-gray-900 to-gray-800 text-white shadow-gray-900/20 absolute grid h-12 w-12 place-items-center">
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor"
+                                aria-hidden="true" class="w-6 h-6 text-white">
+                                <path
+                                    d="M4.5 6.375a4.125 4.125 0 118.25 0 4.125 4.125 0 01-8.25 0zM14.25 8.625a3.375 3.375 0 116.75 0 3.375 3.375 0 01-6.75 0zM1.5 19.125a7.125 7.125 0 0114.25 0v.003l-.001.119a.75.75 0 01-.363.63 13.067 13.067 0 01-6.761 1.873c-2.472 0-4.786-.684-6.76-1.873a.75.75 0 01-.364-.63l-.001-.122zM17.25 19.128l-.001.144a2.25 2.25 0 01-.233.96 10.088 10.088 0 005.06-1.01.75.75 0 00.42-.643 4.875 4.875 0 00-6.957-4.611 8.586 8.586 0 011.71 5.157v.003z">
+                                </path>
+                            </svg>
+                        </div>
+                        <div class="p-4 text-right">
+                            <p class="block antialiased font-sans text-sm leading-normal font-normal text-blue-gray-600">
+                                Groups
+                            </p>
+                            <h4
+                                class="block antialiased tracking-normal font-sans text-2xl font-semibold leading-snug text-blue-gray-900">
+                                4</h4>
+                        </div>
+                        <div class="border-t border-blue-gray-50 p-4">
+                            <p class="block antialiased font-sans text-base leading-relaxed font-normal text-blue-gray-600">
+                                <strong class="text-green-500">+3%</strong>&nbsp;than last month
+                            </p>
+                        </div>
+                    </div>
+
+                    <div
+                        class="dark:bg-gray-700 dark:text-gray-300 dark:border-gray-700 relative flex flex-col bg-clip-border rounded-xl bg-white text-gray-700 border border-blue-gray-100 shadow-sm">
+                        <div
+                            class="bg-clip-border mt-4 mx-4 rounded-xl overflow-hidden bg-gradient-to-tr from-gray-900 to-gray-800 text-white shadow-gray-900/20 absolute grid h-12 w-12 place-items-center">
+                            <i class="fa fa-bookmark"></i>
+                        </div>
+                        <div class="p-4 text-right">
+                            <p class="block antialiased font-sans text-sm leading-normal font-normal text-blue-gray-600">
+                                Booked Events
+                            </p>
+                            <h4
+                                class="block antialiased tracking-normal font-sans text-2xl font-semibold leading-snug text-blue-gray-900">
+                                3,462</h4>
+                        </div>
+                        <div class="border-t border-blue-gray-50 p-4">
+                            <p class="block antialiased font-sans text-base leading-relaxed font-normal text-blue-gray-600">
+                                <strong class="text-red-500">-2%</strong>&nbsp;than yesterday
+                            </p>
+                        </div>
+                    </div>
+
+                    <div
+                        class="dark:bg-gray-700 dark:text-gray-300 dark:border-gray-700 relative flex flex-col bg-clip-border rounded-xl bg-white text-gray-700 border border-blue-gray-100 shadow-sm">
+                        <div
+                            class="bg-clip-border mt-4 mx-4 rounded-xl overflow-hidden bg-gradient-to-tr from-gray-900 to-gray-800 text-white shadow-gray-900/20 absolute grid h-12 w-12 place-items-center">
+                            <i class="fa fa-times"></i>
+                        </div>
+                        <div class="p-4 text-right">
+                            <p class="block antialiased font-sans text-sm leading-normal font-normal text-blue-gray-600">
+                                Cancel Events</p>
+                            <h4
+                                class="block antialiased tracking-normal font-sans text-2xl font-semibold leading-snug text-blue-gray-900">
+                                10</h4>
+                        </div>
+                        <div class="border-t border-blue-gray-50 p-4">
+                            <p class="block antialiased font-sans text-base leading-relaxed font-normal text-blue-gray-600">
+                                <strong class="text-green-500">+5%</strong>&nbsp;than yesterday
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            {{-- Available Events --}}
+            <div class="my-6 bg-white rounded-lg shadow-lg dark:bg-gray-800">
+                <h2 class="px-4 py-5 text-center text-xl md:text-2xl font-semibold text-gray-700 dark:text-gray-300">Available Events</h2>
+            </div>
+
+            <div class="my-6 bg-white rounded-lg shadow-lg dark:bg-gray-800">
+                <div class="grid grid-cols-1 gap-8 md:grid-cols-2 lg:grid-cols-3 p-4">
+                    <!-- Event Card -->
+                    <a href="{{ route('events.info') }}">
+                        <div class="p-6 bg-white rounded-lg shadow-lg dark:bg-gray-700 dark:text-gray-200">
+                            <img class="object-cover w-full h-40 mb-4 rounded" src="/images/africa.jpg" alt="Product Name">
+                            <h3 class="mb-2 text-xl font-semibold">Hododo event center</h3>
+                            <h3 class="mb-2 text-xl font-semibold">NGN. 60,000</h3>
+                            <div class="font-bold">
+                                <p class="text-gray-900 dark:text-gray-100">Title: <span class="text-gray-500 dark:text-gray-400"> December Retreats</span></p>
+                                <p class="text-gray-800 dark:text-gray-100">Description:
+                                    <span class="text-gray-500 dark:text-gray-400">
+                                        orem ipsum dolor sit amet, consectetur adipiscing elit.
+                                    </span>
+                                </p>
+                                <p class="text-gray-800 dark:text-gray-100">Start: <span class="text-gray-500 dark:text-gray-400">22/12/2023</span></p>
+                                <p class="text-gray-800 dark:text-gray-100">End: <span class="text-gray-500 dark:text-gray-400">12/11/2024</span></p>
+                            </div>
+                            <div class="py-2">
+                                <button class="bg-blue-700 hover:bg-blue-800 text-white font-medium py-2 px-4 rounded-lg">
+                                    Book Now
+                                </button>
+                            </div>
+                        </div>
+                    </a>
+
+                    <!-- Event Card -->
+                    <a href="{{ route('events.info') }}">
+                        <div class="p-6 bg-white rounded-lg shadow-lg dark:bg-gray-700 dark:text-gray-200">
+                            <img class="object-cover w-full h-40 mb-4 rounded" src="/images/africa.jpg" alt="Product Name">
+                            <h3 class="mb-2 text-xl font-semibold">Hododo event center</h3>
+                            <h3 class="mb-2 text-xl font-semibold">NGN. 60,000</h3>
+                            <div class="font-bold">
+                                <p class="text-gray-900 dark:text-gray-100">Title: <span class="text-gray-500 dark:text-gray-400"> December Retreats</span></p>
+                                <p class="text-gray-800 dark:text-gray-100">Description:
+                                    <span class="text-gray-500 dark:text-gray-400">
+                                        orem ipsum dolor sit amet, consectetur adipiscing elit.
+                                    </span>
+                                </p>
+                                <p class="text-gray-800 dark:text-gray-100">Start: <span class="text-gray-500 dark:text-gray-400">22/12/2023</span></p>
+                                <p class="text-gray-800 dark:text-gray-100">End: <span class="text-gray-500 dark:text-gray-400">12/11/2024</span></p>
+                            </div>
+                            <div class="py-2">
+                                <button class="bg-yellow-700 hover:bg-yellow-800 text-white font-medium py-2 px-4 rounded-lg">
+                                    View
+                                </button>
+                            </div>
+                        </div>
+                    </a>
+
+                    <!-- Event Card -->
+                    <a href="{{ route('events.info') }}">
+                        <div class="p-6 bg-white rounded-lg shadow-lg dark:bg-gray-700 dark:text-gray-200">
+                            <img class="object-cover w-full h-40 mb-4 rounded" src="/images/africa.jpg" alt="Product Name">
+                            <h3 class="mb-2 text-xl font-semibold">Hododo event center</h3>
+                            <h3 class="mb-2 text-xl font-semibold">NGN. 60,000</h3>
+                            <div class="font-bold">
+                                <p class="text-gray-900 dark:text-gray-100">Title: <span class="text-gray-500 dark:text-gray-400"> December Retreats</span></p>
+                                <p class="text-gray-800 dark:text-gray-100">Description:
+                                    <span class="text-gray-500 dark:text-gray-400">
+                                        orem ipsum dolor sit amet, consectetur adipiscing elit.
+                                    </span>
+                                </p>
+                                <p class="text-gray-800 dark:text-gray-100">Start: <span class="text-gray-500 dark:text-gray-400">22/12/2023</span></p>
+                                <p class="text-gray-800 dark:text-gray-100">End: <span class="text-gray-500 dark:text-gray-400">12/11/2024</span></p>
+                            </div>
+                            <div class="py-2">
+                                <button class="bg-green-700 hover:bg-green-800 text-white font-medium py-2 px-4 rounded-lg">
+                                    Booked
+                                </button>
+                            </div>
+                        </div>
+                    </a>
+
+
+                    <!-- Event Card -->
+                    <a href="{{ route('events.info') }}">
+                        <div class="p-6 bg-white rounded-lg shadow-lg dark:bg-gray-700 dark:text-gray-200">
+                            <img class="object-cover w-full h-40 mb-4 rounded" src="/images/africa.jpg" alt="Product Name">
+                            <h3 class="mb-2 text-xl font-semibold">Hododo event center</h3>
+                            <h3 class="mb-2 text-xl font-semibold">NGN. 60,000</h3>
+                            <div class="font-bold">
+                                <p class="text-gray-900 dark:text-gray-100">Title: <span class="text-gray-500 dark:text-gray-400"> December Retreats</span></p>
+                                <p class="text-gray-800 dark:text-gray-100">Description:
+                                    <span class="text-gray-500 dark:text-gray-400">
+                                        orem ipsum dolor sit amet, consectetur adipiscing elit.
+                                    </span>
+                                </p>
+                                <p class="text-gray-800 dark:text-gray-100">Start: <span class="text-gray-500 dark:text-gray-400">22/12/2023</span></p>
+                                <p class="text-gray-800 dark:text-gray-100">End: <span class="text-gray-500 dark:text-gray-400">12/11/2024</span></p>
+                            </div>
+                            <div class="py-2">
+                                <button class="bg-green-700 hover:bg-green-800 text-white font-medium py-2 px-4 rounded-lg">
+                                    Booked
+                                </button>
+                            </div>
+                        </div>
+                    </a>
+
+                </div>
+            </div>
+            {{-- End of Events --}}
+
+
+        </div>
+
+
+    </div>
+@endsection
