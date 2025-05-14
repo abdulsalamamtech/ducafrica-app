f5f134de9716ef245a510b6076126355f6792f76
abdulsalamamtech <abdulsalamamtech@gmail.com>
2025-05-14 14:50:45 +0100

    Modified log viwer auth provider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -22,21 +22,15 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         LogViewer::auth(function ($request) {
-            // if(!auth()->user()){
-            //     return redirect()->back()->with('error', 'please login');
-            // }
             info('LogViewer Auth', [
                 'time' => now(),
                 'user' => $request?->user(),
                 'role' => $request?->user()?->role,
             ]);
-            return true;
-            // return true to allow viewing the Log Viewer.
-            // return (
-            //     // $request?->user()?->role == UserRoleEnum::ADMIN) ?? $request->user() || 
-            //     in_array($request?->user()?->email, [
-            //     'abdulsalamamtech@gmail.com',
-            // ]) ?? false);
+            return $request->user() && in_array(
+                $request->user()->email, 
+                ['abdulsalamamtech@gmail.com',]
+            );
         });
     }
 }
